8ec1359f57af7355ebd8efadffafa816a3fdaf2e
BibaBlloba <vladvaledol@gmail.com>
2024-09-21 10:15:55 +0300

    nossh

--- fish/config.fish
+++ fish/config.fish
@@ -12,5 +12,4 @@ set -g theme_display_date no
 # alias neofetch='neofetch --source ~/.config/neofetch/art'
 alias n='sudo -E -s nvim'
 alias yt='yt-dlp'
-alias myserv='ssh user@192.168.246.19 -p 7562'
 alias ufw="sudo ufw"
