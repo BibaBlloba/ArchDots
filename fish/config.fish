da512d2b62809e9eb193651ece24b3c83d6fbec9
BibaBlloba <vladvaledol@gmail.com>
2024-10-01 20:44:34 +0300

    fishAlias
    add runc, 120hz, 60hz

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,6 @@ alias n='sudo -E -s nvim'
 alias yt='yt-dlp'
 alias ufw="sudo ufw"
 alias neofetch="neofetch --image_size 250"
+alias runc="g++ -o main main.cpp && ./main"
+alias 120hz="hyprctl keyword monitor eDP-1,1920x1200@120,auto,1.5,"
+alias 60hz="hyprctl keyword monitor eDP-1,1920x1200@60,auto,1.5,"
