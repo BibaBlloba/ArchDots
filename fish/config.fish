29e95068465d5900da9acd86e3670544633d5a67
Akeka_ <vladvaledol@gmail.com>
2025-08-28 22:46:33 +0300

    yazi

--- fish/config.fish
+++ fish/config.fish
@@ -24,3 +24,12 @@ function mkcd
   mkdir -p $argv
   cd $argv
 end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
